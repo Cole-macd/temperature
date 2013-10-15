cf5d6faac948d9036e0eab14c0277eb684aec697
Cole-macd <colemacdonald@vpn164179.wireless.mcgill.ca>
2013-10-15 13:31:14 -0400

    Test getValue method implemented 4 test cases (negative, boundary, casting, and overly precise inputs) for getValue method and created PRECISION variable to apply correct precision

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -8,17 +8,25 @@ import org.junit.* ;
 import static org.junit.Assert.* ;
 
 public class TemperatureTest {
-
-  // Add multiple tests to check all functions of
-  // {@Code Temperature} class.
-
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
-
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+	private final double PRECISION = 0.00001;
+	@Test
+	public void testGetValue() {
+		//test boundary condition, negative input, precision, and double casting inputs
+		
+		//boundary test for negative Kelvin since negative Kelvin is not allowed
+		Temperature testBoundary = new Temperature(0.0, Temperature.Units.KELVIN);
+		assertEquals("Should equal 0.0", 0.0, testBoundary.getValue(), PRECISION);
+		
+		//check for precision of output
+		Temperature precisionTest = new Temperature(0.1111111, Temperature.Units.CELSIUS);
+		assertEquals("Should equal 0.11111", 0.11111, precisionTest.getValue(), PRECISION);
+		
+		//check for cast to double
+		Temperature doubleCasting = new Temperature(10, Temperature.Units.FAHRENHEIT);
+		assertEquals("Should equal 10.0", 10.0, doubleCasting.getValue(), PRECISION);
+		
+		//check negative input
+		Temperature negativeInput = new Temperature(-20, Temperature.Units.CELSIUS);
+		assertEquals("Should equal -20.0", -20.0, negativeInput.getValue(), PRECISION);
+	}
 }
